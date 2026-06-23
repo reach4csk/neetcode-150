cf7b550648b116d3893051f90ff1a6f1f4724ea1
reach4csk <104180099+reach4csk@users.noreply.github.com>
2026-06-23 06:35:53 -0700

    Add: top-k-elements-in-list - submission-19

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-19.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> nmap = new HashMap<>();
+        for (int n : nums) {
+            nmap.merge(n, 1, Integer::sum);
+        }
+
+        List<Integer>[] bucket = new ArrayList[nums.length + 1];
+        for (Map.Entry<Integer, Integer> entry : nmap.entrySet()) {
+            int val = entry.getValue();
+            if (bucket[val] == null) {
+                bucket[val] = new ArrayList<>();
+            }
+            bucket[val].add(entry.getKey());
+        }
+
+        int [] result = new int[k];
+        int i = 0;
+        for(int freq = bucket.length - 1; freq >= 0 && i < k; freq--) {
+            List<Integer> numList = bucket[freq];
+            if (numList != null) {
+                for (int num : numList) {
+                    result[i] = num;
+                    i++;
+                    if (i > k) {
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
