1df0c6811c53d48232047336a53c2d702326dfcc
reach4csk <104180099+reach4csk@users.noreply.github.com>
2026-07-01 08:06:30 -0700

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        char[] carr = s.toCharArray();
+        int len = carr.length;
+        int l=0, r = len-1;
+        while (l < r) {
+            while (l < r && !isAlphaNumber(carr[l])) {
+                l++;
+            }
+            while (r > l && !isAlphaNumber(carr[r])) {
+                r--;
+            }
+            if (Character.toLowerCase(carr[l]) != Character.toLowerCase(carr[r])) {
+                return false;
+            }
+            l++; r--;
+        }
+        return true;
+    }
+    public boolean isAlphaNumber(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') 
+        || (c >= '0' && c <= '9');
+    }
+}
